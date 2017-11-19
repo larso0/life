06d0d7212753bab2a1dc7172417d85bac62ebb98
larso0 <lars.olav.tolo@gmail.com>
2017-11-19 18:19:18 +0100

    updated cmake modules

--- CMakeModules/Findbp.cmake
+++ CMakeModules/Findbp.cmake
@@ -5,19 +5,20 @@
 # BP_LIBRARIES
 
 find_package(Vulkan REQUIRED)
+find_package(shaderc REQUIRED)
 
 find_path(BP_INCLUDE_DIR bp/Instance.h
 	  HINTS ${BP_ROOT_DIR}/include ${BP_ROOT_DIR}/bp/include ${BP_INCLUDE_DIR}
 	  PATHS ../bp/bp/include)
 
 find_library(BP_LIBRARY NAMES bp
-	     HINTS ${BP_ROOT_DIR}/lib ${BP_ROOT_DIR}/cmake-build-debug ${BP_LIBRARY_DIR}
-	     PATHS ../bp/lib ../bp/cmake-build-debug)
+	     HINTS ${BP_ROOT_DIR}/lib ${BP_ROOT_DIR}/cmake-build-debug
+	     PATHS ${BP_LIBRARY_DIR} ../bp/lib ../bp/cmake-build-debug)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(BP DEFAULT_MSG
 				  BP_LIBRARY BP_INCLUDE_DIR)
 mark_as_advanced(BP_INCLUDE_DIR BP_LIBRARY)
 
-set(BP_INCLUDE_DIRS ${BP_INCLUDE_DIR} ${Vulkan_INCLUDE_DIRS})
-set(BP_LIBRARIES ${BP_LIBRARY} ${Vulkan_LIBRARIES})
+set(BP_INCLUDE_DIRS ${BP_INCLUDE_DIR} ${Vulkan_INCLUDE_DIRS} ${SHADERC_INCLUDE_DIRS})
+set(BP_LIBRARIES ${BP_LIBRARY} ${Vulkan_LIBRARIES} ${SHADERC_LIBRARIES})
